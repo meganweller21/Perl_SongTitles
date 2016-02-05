61bdb5f2dbe1f5ab17e19cd0e7326ab25350058e
meganweller21 <mweller213@gmail.com>
2016-02-04 17:38:01 -0700

    Finished steps 1-5

--- weller_bertrand.lab1.pl
+++ weller_bertrand.lab1.pl
@@ -3,13 +3,14 @@
 #										
 #  Megan Weller			
 #  mweller213@gmail.com		
-#	hello									
+#  Ashley Bertrand								
 #########################################
 
 # Replace the string value of the following variable with your names.
 my $name = "Megan Weller";
 my $partner = "Ashley Bertrand";
 print "CSCI 305 Lab 1 submitted by $name and $partner.\n\n";
+
 
 # Checks for the argument, fail if none given
 if($#ARGV != 0) {
@@ -26,16 +27,9 @@ open(INFILE, $ARGV[0]) or die "Cannot open $ARGV[0]: $!.\n";
 # This loops through each line of the file
 while($line = <INFILE>) {
 
-	# This prints each line. You will not want to keep this line.
-	#print $line;
-
-#Example:
-#Carl Davis Alice in Wonderland: Act I: The Caucus Race
-
-
 	# YOUR CODE BELOW...
 	# Step 1, remove the extra strings before and between the song title
-		#each part of the string
+
 		my $s = $line =~ /\%([A-Z0-9]*)(\<[SEP]+\>)([A-Z0-9]*)(\<[SEP]+\>)(.*)(\<[SEP]+\>)(.*)/;
 		print "\n";
 		
@@ -43,36 +37,37 @@ while($line = <INFILE>) {
 		$line =~ s/\%$1$2$3$4$5$6//g;
 		#rename line to title
 		$title = $line;
-		print $title, "\n";
 	
 	# Step 2 and 3, remove punctuation and symbols
 
-	foreach("\n", $title){
-		#this works! Gets rid of symbols	
+		#Gets rid of symbols	
 		$title =~ s/([¿?¡!.&\$#@\%|])+//g;
-		
-		#Working and experimenting here, not sure how to get rid of everything between the (),etc.
-		#$title =~ s/([\[\({\-_\+*;:`\\\/])+//;
-		
-		#testing with this, it will delete the / but nothing after
-		#Not sure why it's printing so much!
-		$title =~ s/(\/(^*))+//g;
-		
-		$newTitle = $title;
-		
-		print $newTitle;
-	}
 
-
-
-
-
-	# Step 4
+		#( { \ / -: "‘+=*feat.
+		$title =~ s/(\/(.*))+//g;
+		$title =~ s/(\\(.*))+//g;
+		$title =~ s/(\-(.*))+//g;
+		$title =~ s/(_(.*))+//g;
+		$title =~ s/(\:(.*))+//g;
+		$title =~ s/(\"(.*))+//g;
+		$title =~ s/(\`(.*))+//g;
+		$title =~ s/(\+(.*))+//g;
+		$title =~ s/(\=(.*))+//g;
+		$title =~ s/(\*(.*))+//g;
+		$title =~ s/(feat.)+//g;
+		$title =~ s/(\((.*)\))+//g;
+		$title =~ s/(\()+//g;
+		$title =~ s/(\[(.*)\])+//g;
+		$title =~ s/(\{(.*)\})+//g;
 
 	# Step 5, convert to lowercase
-	$lowerCase = lc $title;
-	print $lowerCase;
-	
+	$newTitle = lc $title;
+
+	# Step 4
+	foreach($title){
+
+		print $newTitle unless /[^[:ascii:]]/;
+	}
 	
 }
 
