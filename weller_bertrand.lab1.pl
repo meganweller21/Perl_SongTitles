053022b202bed94479539c590abd628784957122
ashleybertrand <ashleykaybertrand@gmail.com>
2016-02-14 22:38:29 -0700

    Created bigram data structure using hash of hashes

--- weller_bertrand.lab1.pl
+++ weller_bertrand.lab1.pl
@@ -6,8 +6,6 @@
 #  Ashley Bertrand								
 #########################################
 
-use Data::Dumper;
-
 # Replace the string value of the following variable with your names.
 my $name = "Megan Weller";
 my $partner = "Ashley Bertrand";
@@ -25,6 +23,9 @@ open(INFILE, $ARGV[0]) or die "Cannot open $ARGV[0]: $!.\n";
 # YOUR VARIABLE DEFINITIONS HERE...
 my $numValid = 0;
 my $titles;
+my %hash;
+my @keys;
+my @values;
 
 # This loops through each line of the file
 while($line = <INFILE>) {
@@ -56,11 +57,9 @@ while($line = <INFILE>) {
 	$title =~ s/([?¿!¡.;&\$@\%#|])+//g;
 
 	# Step 4, filter out non-English characters
-	if ($title =~ /[^[:ascii:]]/) {
-		#print("invalid: $title\n");
+	if ($title =~ /[^[:ascii:]]/ or $title =~ /~/) {
 		$title = "";
 	} else {
-		#print("valid: $title\n");
 		$numValid = $numValid + 1;
 	}
 
@@ -69,20 +68,14 @@ while($line = <INFILE>) {
 
 	#making a titles array where each index is a title
 	@titles[numValid] = $title;
-	#print("title: $titles[numValid]");
 
 	#splitting each title into words
 	my @words = split ' ', $titles[numValid];
-	#foreach $word (@words) {
-	#	print("word: $word\n");
-	#}
 
-	my @keys;
-	my @values;
-	my %hash;
 	my $i = 0;
 
-	for($index=0; $index <= $#words - 1; $index++){
+	#use words - 1 so that the last word in a title is not included as a bigram with an empty string
+	for($index=0; $index <= $#words - 1; $index++) {
 
 		#array to hold keys
     	@keys[i] = $words[$index];
@@ -92,27 +85,21 @@ while($line = <INFILE>) {
 
     	#print("key: @keys[i], value: @values[i]\n");
 
-    	#for($j=0; $j <= $#keys; $j++) {
-		#	print("key: $keys[j], ");
-		#}
+    	#building hash of ((key, value), count)
+    	for($j=0; $j <= $#keys; $j++) {
+			for ($k = 0; $k < scalar @keys; $k++) {
+   				$hash{$keys[$k]}{$values[$k]} += 1;
+   			}
+		}		
 
-		#for($j=0; $j <= $#values; $j++) {
-		#	print("value: $values[j]\n");
-		#}
-
-		#hash of keys and values
-		@hash{@keys} = @values;
-
-    	$i = $i + 1;
+		$i = $i + 1;
 	}
 
-	
-
-#=pod
+=pod
 	#<=> makes it so keys are treated as numbers,
 	#keys get sorted numerically
 	foreach my $key(sort {$hash{$b}<=>$hash{$a}}keys %hash) {
-		printf "$key\t$hash{$key}\n";
+		#printf "$key\t$hash{$key}\n";
 
 		if(! %hash) {
 			print("in first if statement");
@@ -128,10 +115,17 @@ while($line = <INFILE>) {
 
 	  	}
 	}
-#=cut
+=cut
 }
 
-print("\n\nThere are $numValid valid lines.\n");
+#print key : value = count
+foreach my $key (sort keys %hash) {
+   	while (my ($value, $count) = each %{$hash{$key}}) {
+    	print "$key : $value = $count\n";
+   	}
+}
+
+print("\nThere are $numValid valid lines.\n");
 
 # Close the file handle
 close INFILE; 
@@ -140,6 +134,20 @@ close INFILE;
 # title file and have populated your data structure of bigram counts.
 print "\nFile parsed. Bigram model built.\n\n";
 
+#returns the word that most commonly follows the given argument
+sub function() {
+	$key = $_[0];
+	if(exists($hash{$key})) {
+		while (my ($value, $count) = each %{$hash{$key}}) {
+			#doesn't work
+   		}
+
+	} else {
+		return "$key does not exist";
+	}
+}
+
+print &mcw("all");
 
 # User control loop
 #print "Enter a word [Enter 'q' to quit]: ";
